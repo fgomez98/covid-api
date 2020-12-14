7ec580f08d6fde797ff810d0cde439d6c6fabd50
Ignacio <igrasso@itba.edu.ar>
2020-12-14 11:08:13 -0300

    delete unused code

--- config.py
+++ config.py
@@ -4,11 +4,6 @@ class Config(object):
 
     DATA_FILE = 'data/Covid19Casos100000.csv'
 
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://db:username@localhost/dbname'
-    # DB = ""
-    # DB_USERNAME = ""
-    # DB_PASSWD = ""
-
 
 class ProductionConfig(Config):
     pass
